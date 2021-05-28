263223a7b5f4c577238a1cd456a3276bc91703f6
Gautam Gupta <gautamgupta9298@gmail.com>
2021-05-28 10:38:12 -0400

    Improved collision detection and added draw board

--- board.js
+++ board.js
@@ -1,23 +1,58 @@
 class Board {
+    constructor(ctx, ctxNext) {
+        this.ctx = ctx;
+        this.ctxNext = ctxNext;
+        this.init();
+    }
+
+    init() {
+        this.ctx.canvas.width = COLS * BLOCK_SIZE;
+        this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+        this.ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+    }
+
     reset = () => {
-        this.grid = this.getEmptyBoard()
+        this.grid = this.getEmptyBoard();
+        this.piece = new Piece(this.ctx);
+        this.piece.setStartingPosition();
+    }
+
+    draw = () => {
+        this.piece.draw();
+        this.drawBoard();
+    }
+
+    drawBoard() {
+        this.grid.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value > 0) {
+                    this.ctx.fillStyle = COLORS[value];
+                    this.ctx.fillRect(x, y, 1, 1);
+                }
+            });
+        });
     }
 
     getEmptyBoard = () => {
         return Array.from({ length: ROWS }, () => Array(COLS).fill(0))
     }
 
+    isInsideWalls(x, y) {
+        return x >= 0 && x < COLS && y <= ROWS;
+    }
 
-    valid = (p) => {
+    notOccupied(x, y) {
+        return this.grid[y] && this.grid[y][x] === 0;
+    }
+
+
+    valid(p) {
         return p.shape.every((row, dy) => {
             return row.every((value, dx) => {
                 let x = p.x + dx;
                 let y = p.y + dy;
-                return (
-                    this.isEmpty(value) ||
-                    (this.insideWalls(x) &&
-                        this.aboveFloor(y)
-                    ));
+                return value === 0 || (this.isInsideWalls(x, y) && this.notOccupied(x, y));
             });
         });
     }
